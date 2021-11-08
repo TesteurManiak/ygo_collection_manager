ed7094f969048789b21265f31dc278b3caea0a80
Guillaume Roux <rouxguillaume8@gmail.com>
2021-11-08 01:08:19 +0100

    updated service locator

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -4,12 +4,37 @@ import 'data/api/api.dart';
 import 'data/datasources/local/hive/ygopro_local_datasource_hive.dart';
 import 'data/datasources/local/ygopro_local_datasource.dart';
 import 'data/datasources/remote/ygopro_remote_data_source.dart';
+import 'data/repository/ygopro_repository_impl.dart';
+import 'domain/repository/ygopro_repository.dart';
 
 final sl = GetIt.instance;
 
 void setupLocator() {
-  sl.registerSingleton<RemoteClient>(DioClient());
+  //! Bloc
+  // sl.registerFactory();
 
-  sl.registerSingleton(YgoProRemoteDataSource(sl()));
-  sl.registerSingleton<YgoProLocalDataSource>(YgoProLocalDataSourceHive());
+  //! Domain
+  // Use cases
+  // sl.registerLazySingleton();
+
+  // Repository
+  sl.registerLazySingleton<YgoProRepository>(
+    () => YgoProRepositoryImpl(
+      remoteDataSource: sl(),
+      localDataSource: sl(),
+    ),
+  );
+
+  //! Data
+  // Data sources
+  sl.registerLazySingleton(() => YgoProRemoteDataSource(sl()));
+  sl.registerLazySingleton<YgoProLocalDataSource>(
+    () => YgoProLocalDataSourceHive(),
+  );
+
+  //! Core
+  // sl.registerLazySingleton();
+
+  //! External
+  sl.registerLazySingleton<RemoteClient>(() => DioClient());
 }
