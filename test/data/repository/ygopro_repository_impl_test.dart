589f15509a528045012fc0108b23063d77695071
Guillaume Roux <rouxguillaume8@gmail.com>
2021-11-20 01:41:14 +0100

    added unit test

--- test/data/repository/ygopro_repository_impl_test.dart
+++ test/data/repository/ygopro_repository_impl_test.dart
@@ -4,6 +4,7 @@ import 'package:mockito/mockito.dart';
 import 'package:ygo_collection_manager/core/platform/network_info.dart';
 import 'package:ygo_collection_manager/data/datasources/local/ygopro_local_datasource.dart';
 import 'package:ygo_collection_manager/data/datasources/remote/ygopro_remote_data_source.dart';
+import 'package:ygo_collection_manager/data/models/response/ygo_set_model.dart';
 import 'package:ygo_collection_manager/data/repository/ygopro_repository_impl.dart';
 
 import 'ygopro_repository_impl_test.mocks.dart';
@@ -21,16 +22,33 @@ void main() {
   );
 
   group('getAllSets', () {
+    final tSets = [
+      YgoSetModel(numOfCards: 1, setCode: '', setName: '', tcgDate: null),
+    ];
+
     test('should check if device is online', () async {
       // arrange
       when(mockNetworkInfo.isConnected).thenAnswer((_) async => true);
-      when(mockRemoteDataSource.getAllSets()).thenAnswer((_) async => []);
+      when(mockRemoteDataSource.getAllSets()).thenAnswer((_) async => tSets);
 
       // act
       repository.getAllSets();
 
       // assert
       verify(mockNetworkInfo.isConnected);
+    });
+
+    test('should return remote data when device is online', () async {
+      // arrange
+      when(mockNetworkInfo.isConnected).thenAnswer((_) async => true);
+      when(mockRemoteDataSource.getAllSets()).thenAnswer((_) async => tSets);
+
+      // act
+      final result = await repository.getAllSets();
+
+      // assert
+      expect(result, tSets);
+      verify(mockRemoteDataSource.getAllSets());
     });
   });
 }
