542ae3c642a61675c956af6f1d02ded93c33cf4f
Guillaume Roux <rouxguillaume8@gmail.com>
2021-11-23 16:21:54 +0100

    removed unused method in repository and added unit test for getAllCards

--- lib/domain/repository/ygopro_repository.dart
+++ lib/domain/repository/ygopro_repository.dart
@@ -1,5 +1,4 @@
 import '../entities/card_owned.dart';
-import '../entities/card_set_info.dart';
 import '../entities/db_version.dart';
 import '../entities/ygo_card.dart';
 import '../entities/ygo_set.dart';
@@ -20,9 +19,6 @@ abstract class YgoProRepository {
   ///
   /// If the device is offline, fetch a the cards from the local datasource.
   Future<List<YgoCard>> getAllCards({required bool shouldReload});
-
-  /// Returns a [CardSetInfo] for the given [setCode] from the API.
-  Future<CardSetInfo> getCardSetInformation(String setCode);
 
   Future<DbVersion> checkDatabaseVersion();
 
