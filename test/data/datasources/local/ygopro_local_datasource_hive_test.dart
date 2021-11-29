295a681cf1ac0ba7cd11f563c9810bbcc06e8090
Guillaume Roux <rouxguillaume8@gmail.com>
2021-11-29 16:14:36 +0100

    mocked hive db

--- test/data/datasources/local/ygopro_local_datasource_hive_test.dart
+++ test/data/datasources/local/ygopro_local_datasource_hive_test.dart
@@ -1,16 +1,68 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 import 'package:ygo_collection_manager/data/datasources/local/hive/ygopro_local_datasource_hive.dart';
+import 'package:ygo_collection_manager/domain/entities/card_owned.dart';
+import 'package:ygo_collection_manager/domain/entities/db_version.dart';
+import 'package:ygo_collection_manager/domain/entities/ygo_card.dart';
+import 'package:ygo_collection_manager/domain/entities/ygo_set.dart';
 
 import 'ygopro_local_datasource_hive_test.mocks.dart';
 
-@GenerateMocks([HiveInterface])
+class MockYgoCardBox extends MockBox<YgoCard> {}
+
+class MockYgoSetBox extends MockBox<YgoSet> {}
+
+class MockDbVersionBox extends MockBox<DbVersion> {}
+
+class MockCardOwnedBox extends MockBox<CardOwned> {}
+
+@GenerateMocks([HiveInterface, Box])
 void main() {
   final mockHiveInterface = MockHiveInterface();
   final dataSource = YgoProLocalDataSourceHive(hive: mockHiveInterface);
 
-  group('initDb', () {});
+  group('initDb', () {
+    test('should register adapters and open the boxes', () async {
+      // arrange
+      when(mockHiveInterface.registerAdapter(any)).thenAnswer((_) {});
+      when(
+        mockHiveInterface
+            .openBox<YgoCard>(YgoProLocalDataSourceHive.tableCards),
+      ).thenAnswer((_) async => MockYgoCardBox());
+      when(
+        mockHiveInterface.openBox<YgoSet>(YgoProLocalDataSourceHive.tableSets),
+      ).thenAnswer((_) async => MockYgoSetBox());
+      when(
+        mockHiveInterface.openBox<DbVersion>(YgoProLocalDataSourceHive.tableDB),
+      ).thenAnswer((_) async => MockDbVersionBox());
+      when(
+        mockHiveInterface
+            .openBox<CardOwned>(YgoProLocalDataSourceHive.tableCardsOwned),
+      ).thenAnswer((_) async => MockCardOwnedBox());
+
+      // act
+      await dataSource.initDb();
+
+      // assert
+      verify(mockHiveInterface.registerAdapter(any));
+      verify(
+        mockHiveInterface
+            .openBox<YgoCard>(YgoProLocalDataSourceHive.tableCards),
+      );
+      verify(
+        mockHiveInterface.openBox<YgoSet>(YgoProLocalDataSourceHive.tableSets),
+      );
+      verify(
+        mockHiveInterface.openBox<DbVersion>(YgoProLocalDataSourceHive.tableDB),
+      );
+      verify(
+        mockHiveInterface
+            .openBox<CardOwned>(YgoProLocalDataSourceHive.tableCardsOwned),
+      );
+    });
+  });
 
   group('closeDb', () {});
 
--- test/data/datasources/local/ygopro_local_datasource_hive_test.mocks.dart
+++ test/data/datasources/local/ygopro_local_datasource_hive_test.mocks.dart
@@ -133,3 +133,124 @@ class MockHiveInterface extends _i1.Mock implements _i2.HiveInterface {
       super.noSuchMethod(Invocation.method(#ignoreTypeId, [typeId]),
           returnValueForMissingStub: null);
 }
+
+/// A class which mocks [Box].
+///
+/// See the documentation for Mockito's code generation for more information.
+class MockBox<E> extends _i1.Mock implements _i2.Box<E> {
+  MockBox() {
+    _i1.throwOnMissingStub(this);
+  }
+
+  @override
+  Iterable<E> get values =>
+      (super.noSuchMethod(Invocation.getter(#values), returnValue: <E>[])
+          as Iterable<E>);
+  @override
+  String get name =>
+      (super.noSuchMethod(Invocation.getter(#name), returnValue: '') as String);
+  @override
+  bool get isOpen =>
+      (super.noSuchMethod(Invocation.getter(#isOpen), returnValue: false)
+          as bool);
+  @override
+  bool get lazy =>
+      (super.noSuchMethod(Invocation.getter(#lazy), returnValue: false)
+          as bool);
+  @override
+  Iterable<dynamic> get keys =>
+      (super.noSuchMethod(Invocation.getter(#keys), returnValue: <dynamic>[])
+          as Iterable<dynamic>);
+  @override
+  int get length =>
+      (super.noSuchMethod(Invocation.getter(#length), returnValue: 0) as int);
+  @override
+  bool get isEmpty =>
+      (super.noSuchMethod(Invocation.getter(#isEmpty), returnValue: false)
+          as bool);
+  @override
+  bool get isNotEmpty =>
+      (super.noSuchMethod(Invocation.getter(#isNotEmpty), returnValue: false)
+          as bool);
+  @override
+  Iterable<E> valuesBetween({dynamic startKey, dynamic endKey}) =>
+      (super.noSuchMethod(
+          Invocation.method(
+              #valuesBetween, [], {#startKey: startKey, #endKey: endKey}),
+          returnValue: <E>[]) as Iterable<E>);
+  @override
+  E? getAt(int? index) =>
+      (super.noSuchMethod(Invocation.method(#getAt, [index])) as E?);
+  @override
+  Map<dynamic, E> toMap() => (super.noSuchMethod(Invocation.method(#toMap, []),
+      returnValue: <dynamic, E>{}) as Map<dynamic, E>);
+  @override
+  String toString() => super.toString();
+  @override
+  dynamic keyAt(int? index) =>
+      super.noSuchMethod(Invocation.method(#keyAt, [index]));
+  @override
+  _i3.Stream<_i2.BoxEvent> watch({dynamic key}) => (super.noSuchMethod(
+      Invocation.method(#watch, [], {#key: key}),
+      returnValue: Stream<_i2.BoxEvent>.empty()) as _i3.Stream<_i2.BoxEvent>);
+  @override
+  bool containsKey(dynamic key) =>
+      (super.noSuchMethod(Invocation.method(#containsKey, [key]),
+          returnValue: false) as bool);
+  @override
+  _i3.Future<void> put(dynamic key, E? value) =>
+      (super.noSuchMethod(Invocation.method(#put, [key, value]),
+          returnValue: Future<void>.value(),
+          returnValueForMissingStub: Future<void>.value()) as _i3.Future<void>);
+  @override
+  _i3.Future<void> putAt(int? index, E? value) =>
+      (super.noSuchMethod(Invocation.method(#putAt, [index, value]),
+          returnValue: Future<void>.value(),
+          returnValueForMissingStub: Future<void>.value()) as _i3.Future<void>);
+  @override
+  _i3.Future<void> putAll(Map<dynamic, E>? entries) =>
+      (super.noSuchMethod(Invocation.method(#putAll, [entries]),
+          returnValue: Future<void>.value(),
+          returnValueForMissingStub: Future<void>.value()) as _i3.Future<void>);
+  @override
+  _i3.Future<int> add(E? value) =>
+      (super.noSuchMethod(Invocation.method(#add, [value]),
+          returnValue: Future<int>.value(0)) as _i3.Future<int>);
+  @override
+  _i3.Future<Iterable<int>> addAll(Iterable<E>? values) =>
+      (super.noSuchMethod(Invocation.method(#addAll, [values]),
+              returnValue: Future<Iterable<int>>.value(<int>[]))
+          as _i3.Future<Iterable<int>>);
+  @override
+  _i3.Future<void> delete(dynamic key) =>
+      (super.noSuchMethod(Invocation.method(#delete, [key]),
+          returnValue: Future<void>.value(),
+          returnValueForMissingStub: Future<void>.value()) as _i3.Future<void>);
+  @override
+  _i3.Future<void> deleteAt(int? index) =>
+      (super.noSuchMethod(Invocation.method(#deleteAt, [index]),
+          returnValue: Future<void>.value(),
+          returnValueForMissingStub: Future<void>.value()) as _i3.Future<void>);
+  @override
+  _i3.Future<void> deleteAll(Iterable<dynamic>? keys) =>
+      (super.noSuchMethod(Invocation.method(#deleteAll, [keys]),
+          returnValue: Future<void>.value(),
+          returnValueForMissingStub: Future<void>.value()) as _i3.Future<void>);
+  @override
+  _i3.Future<void> compact() =>
+      (super.noSuchMethod(Invocation.method(#compact, []),
+          returnValue: Future<void>.value(),
+          returnValueForMissingStub: Future<void>.value()) as _i3.Future<void>);
+  @override
+  _i3.Future<int> clear() => (super.noSuchMethod(Invocation.method(#clear, []),
+      returnValue: Future<int>.value(0)) as _i3.Future<int>);
+  @override
+  _i3.Future<void> close() => (super.noSuchMethod(Invocation.method(#close, []),
+      returnValue: Future<void>.value(),
+      returnValueForMissingStub: Future<void>.value()) as _i3.Future<void>);
+  @override
+  _i3.Future<void> deleteFromDisk() =>
+      (super.noSuchMethod(Invocation.method(#deleteFromDisk, []),
+          returnValue: Future<void>.value(),
+          returnValueForMissingStub: Future<void>.value()) as _i3.Future<void>);
+}
