12928e3f92131560b2aa92bbf0912099b9d67710
Guillaume Roux <rouxguillaume8@gmail.com>
2021-12-21 23:07:59 +0100

    simplified string getter for enum Sort

--- lib/data/models/request/get_card_info_request.dart
+++ lib/data/models/request/get_card_info_request.dart
@@ -111,20 +111,10 @@ extension LinkMarkersModifier on LinkMarkers {
 extension SortModifier on Sort {
   String get string {
     switch (this) {
-      case Sort.atk:
-        return 'atk';
-      case Sort.def:
-        return 'def';
-      case Sort.name:
-        return 'name';
-      case Sort.type:
-        return 'type';
-      case Sort.level:
-        return 'level';
-      case Sort.id:
-        return 'id';
       case Sort.newest:
         return 'new';
+      default:
+        return name;
     }
   }
 }
