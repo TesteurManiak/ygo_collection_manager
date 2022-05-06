572623561b3b4629516cd78d09cea342b29d75b3
Guillaume Roux <rouxguillaume8@gmail.com>
2022-05-06 13:07:40 +0200

    fixed router test

--- test/domain/usecases/fetch_all_cards_test.dart
+++ test/domain/usecases/fetch_all_cards_test.dart
@@ -1,17 +1,18 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/annotations.dart';
-import 'package:mockito/mockito.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:ygo_collection_manager/domain/entities/ygo_card.dart';
-import 'package:ygo_collection_manager/domain/repository/ygopro_repository.dart';
 import 'package:ygo_collection_manager/domain/usecases/fetch_all_cards.dart';
 
-import 'fetch_all_cards_test.mocks.dart';
+import '../../utils/mocks.dart';
 
-@GenerateMocks([YgoProRepository])
 void main() {
-  final mockRepository = MockYgoProRepository();
+  late MockYgoProRepository mockRepository;
+  late FetchAllCards useCase;
 
-  final useCase = FetchAllCards(mockRepository);
+  setUp(() {
+    mockRepository = MockYgoProRepository();
+    useCase = FetchAllCards(mockRepository);
+  });
 
   group('FetchAllCards', () {
     const tShouldReload = false;
@@ -19,14 +20,14 @@ void main() {
 
     test('Should call getAllCards from the repository', () async {
       // arrange
-      when(mockRepository.getAllCards(shouldReload: tShouldReload))
+      when(() => mockRepository.getAllCards(shouldReload: tShouldReload))
           .thenAnswer((_) async => tCards);
 
       // act
       final result = await useCase(shouldReload: tShouldReload);
 
       // assert
-      verify(mockRepository.getAllCards(shouldReload: tShouldReload));
+      verify(() => mockRepository.getAllCards(shouldReload: tShouldReload));
       expect(result, tCards);
     });
   });
--- test/utils/mocks.dart
+++ test/utils/mocks.dart
@@ -9,6 +9,11 @@ import 'package:ygo_collection_manager/domain/entities/card_owned.dart';
 import 'package:ygo_collection_manager/domain/entities/db_version.dart';
 import 'package:ygo_collection_manager/domain/entities/ygo_card.dart';
 import 'package:ygo_collection_manager/domain/entities/ygo_set.dart';
+import 'package:ygo_collection_manager/domain/repository/ygopro_repository.dart';
+import 'package:ygo_collection_manager/domain/usecases/fetch_all_cards.dart';
+import 'package:ygo_collection_manager/domain/usecases/fetch_all_sets.dart';
+import 'package:ygo_collection_manager/domain/usecases/fetch_owned_cards.dart';
+import 'package:ygo_collection_manager/domain/usecases/should_reload_db.dart';
 
 class MockConnectivity extends Mock implements Connectivity {}
 
@@ -30,3 +35,13 @@ class MockYgoProRemoteDataSource extends Mock
 class MockYgoProLocalDataSource extends Mock implements YgoProLocalDataSource {}
 
 class MockNetworkInfo extends Mock implements NetworkInfo {}
+
+class MockYgoProRepository extends Mock implements YgoProRepository {}
+
+class MockFetchAllSets extends Mock implements FetchAllSets {}
+
+class MockFetchAllCards extends Mock implements FetchAllCards {}
+
+class MockFetchOwnedCards extends Mock implements FetchOwnedCards {}
+
+class MockShouldReloadDb extends Mock implements ShouldReloadDb {}
